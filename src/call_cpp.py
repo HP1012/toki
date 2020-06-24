19d4d55da6d043ff391928abdc2cbc80b839a27c
HP1012 <hp1012@DESKTOP-9OHF6BD.localdomain>
2020-06-24 20:58:59 +0700

    update fic

--- src/call_cpp.py
+++ src/call_cpp.py
@@ -3,12 +3,14 @@ import os
 from pathlib import Path
 
 NAME = './sql.so'
+PATH = str(Path.cwd()) + "/db/demo.db"
 if "src" not in str(Path.cwd()):
     NAME = './src/' + NAME.replace('./','')
+    PATH = str(Path.cwd()) + "/src/db/demo.db"
 
 ffi = cffi.FFI()
 ffi.cdef("""
-    int cffi_insert_database(int id,const char name[],int age,const char address[],int salary);
+    int cffi_insert_database(const char path[],int id,const char name[],int age,const char address[],int salary);
     """)
 try:
     C = ffi.dlopen(NAME)
@@ -22,12 +24,12 @@ def py_hello(name):
     return C.cffi_hello(name)
 
 def main(id,name,age,address,salary):
-    name_arr = []
-    name_arr += [target_list for target_list in name]
-    address_arr = []
-    address_arr += [target_list for target_list in address]
+    # name_arr = []
+    # name_arr += [target_list for target_list in name]
+    # address_arr = []
+    # address_arr += [target_list for target_list in address]
 
-    tmp = C.cffi_insert_database(id,name_arr,age,address_arr,salary)
+    tmp = C.cffi_insert_database(PATH.encode('ascii'),id,name.encode('ascii'),age,address.encode('ascii'),salary)
     print("Call c function success {0}".format(tmp))
 
 
--- src/sql.cpp
+++ src/sql.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "libsqlite3/sqlite3.h"
 
+
+
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    int i;
    for(i = 0; i<argc; i++) {
@@ -11,14 +13,15 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    return 0;
 }
 
-int insert_database(int id,const char name[],int age,const char address[],int salary){
+int insert_database(const char path[],int id,const char name[],int age,const char address[],int salary){
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
    char *sql = (char*)malloc(10000);
 
    /* Open database */
-   rc = sqlite3_open("db/demo.db", &db);
+   fprintf(stderr,"Open database path: %s\n",path);
+   rc = sqlite3_open(path, &db);
    
    if( rc ) {
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
@@ -50,13 +53,13 @@ int insert_database(int id,const char name[],int age,const char address[],int sa
 }
 
 int main(int argc, char* argv[]) {
-   int tmp = insert_database(1,"phi",24,"VietNam",500);
+   int tmp = insert_database("db/demo.db",1,"phi",24,"VietNam",500);
    return 0;
 }
 
 extern "C"{
-   int cffi_insert_database(int id,const char name[],int age,const char address[],int salary){
-      int tmp = insert_database(id,name,age,address,salary);
+   int cffi_insert_database(const char path[],int id,const char name[],int age,const char address[],int salary){
+      int tmp = insert_database(path,id,name,age,address,salary);
       return tmp;
    }
 }
